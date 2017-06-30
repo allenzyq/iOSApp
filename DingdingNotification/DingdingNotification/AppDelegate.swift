d9c493231a57cc55aae4716dee3084b9843ca6cd
zyq <allenzyq1314@163.com>
2017-06-30 09:34:14 +0800

    商学院代码迁移coding success pod update build success run success

--- DingdingNotification/DingdingNotification/AppDelegate.swift
+++ DingdingNotification/DingdingNotification/AppDelegate.swift
@@ -14,6 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 // init Test11
+    ///
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
